b395227409c47c7d95698aee900995a34d006324
Dey <dibyendu.dey@intel.com>
2020-06-06 00:18:05 -0700

    adding config class file

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config():
+    """
+    Setting all configuration variable as the class attributes instead of doing as below:
+        app.config['SECRET_KEY'] = 'you-will-never-guess'
+    """
+    SECRET_KEY= "you-will-never-guess"
