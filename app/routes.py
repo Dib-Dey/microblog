d6db995e1654b08d62fb8dadb1bc2b09181aa95b
Dey <dibyendu.dey@intel.com>
2020-06-08 22:07:03 -0700

    Adding the GET and POST variables

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import flask_app
 from flask import render_template
-from app.flask_form import LoginForm
+from flask import flash
+from flask import redirect
+from app.form import LoginForm
 
 #the route() decorator to tell Flask what URL should trigger our function.
 @flask_app.route('/')
@@ -10,7 +12,10 @@ def index():
     title = "Flask Example:"
     return render_template('index.html', title=title, user= user)
 
-@flask_app.route('/form')
-def form():
+@flask_app.route('/form', methods = ['GET', 'POST'])
+def log():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('form.html',form = form, title="Login Form")
